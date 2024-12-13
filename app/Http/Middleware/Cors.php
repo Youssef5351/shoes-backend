aee9e5d9ffee1bd7fe8a7eb2c7b739d8ed51f4ea
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-14 00:03:37 +0200

    Update Cors.php

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://shoes1-omega.vercel.app')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
