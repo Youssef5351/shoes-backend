93d8a665f0b5de80831f06574fe2104f0eaa80b5
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-14 00:14:02 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->appendToGroup('api', \App\Http\Middleware\CorsMiddleware::class);
+
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->append(function ($request, $next) {
+        $response = $next($request);
+        
+        $response->headers->set('Access-Control-Allow-Origin', 'https://shoes1-omega.vercel.app');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        
+        return $response;
+    });
 })
     ->withExceptions(function (Exceptions $exceptions) {
         //
