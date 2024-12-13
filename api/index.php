dd0d8cc031f5d16d62b7e83e649c0bfb9de6dc27
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-13 05:14:35 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,10 @@
-<?php
-
-// Load the Laravel autoload file
-require __DIR__.'/../vendor/autoload.php';
-
-// Set up the Laravel application
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Set up the routing
+DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+ 
+// Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// Handle the request
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
-// Send the response
 $response->send();
-
-// Terminate the request
 $kernel->terminate($request, $response);
