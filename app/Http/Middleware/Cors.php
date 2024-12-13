da84e3a4084ab2f7cdabac83c88669ef947a114b
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-14 00:21:57 +0200

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,9 +10,14 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://shoes1-omega.vercel.app')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        if ($request->is('api/*')) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', 'https://shoes1-omega.vercel.app')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+                ->header('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $next($request);
     }
 }
