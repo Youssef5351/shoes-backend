749d9e54e0835650ac00d3e6d6f469acb450a6fd
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-13 21:13:56 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CheckoutController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-// routes/web.php
-Route::post('/create-checkout-session', [CheckoutController::class, 'createCheckoutSession']);
+
+
+Route::get('/shopify-products', [ProductController::class, 'getShopifyProducts']);
+Route::get('/featured-products', [ProductController::class, 'getFeaturedProducts']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::get('/products/{id}', [ProductController::class, 'getShopifyProductDetails']);
+Route::post('/checkout', [CheckoutController::class, 'createCheckout']);
