65166af46b07acf3c3d7fb3b3cf4cfac80e244ca
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-13 05:14:57 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
-DIR__ . '/../vendor/autoload.php';
+<?php
+ 
+// Load the Laravel application
+require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
  
 // Run the application
