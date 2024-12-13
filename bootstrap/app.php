efeca43608b21c90be18e01c2d63cec7ff715ba5
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-12-14 00:04:21 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
-    })
+    $middleware->appendToGroup('api', \App\Http\Middleware\CorsMiddleware::class);
+})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
